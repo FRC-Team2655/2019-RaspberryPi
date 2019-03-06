03f694d190d1b8eeb24f353550140d56e9e42c5f
Marcus Behel <marcus.behel@mindspring.com>
2019-03-06 09:34:35 -0500

    Organized camera stream setup

--- camera-stream/start-camerastream.sh
+++ camera-stream/start-camerastream.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-
-touch /home/pi/camera-server-started.txt
-
-# Make sure to kill background processes on exit
-trap "kill 0" EXIT
-
 # Directory of this script
 SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a syml$
   DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative syml$
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
-HTTPPOIRT=5805
-
-fuser -k $HTTPPORT/tcp
-
-ffserver -f $DIR/ffserver.conf &
-ffmpeg -f v4l2 -s 480x360 -r 15 -input_format mjpeg -i /dev/video0 -crf 30 -an http://localhost:5805/camera0.ffm &
-ffmpeg -f v4l2 -s 480x360 -r 15 -input_format mjpeg -i /dev/video1 -crf 30 -an http://localhost:5805/camera1.ffm &
-
-wait
+$DIR/mjpg/start-mjpg.sh
